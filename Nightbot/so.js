60e3beb408c4d0fbe2f6576e7f79083be39c6c5c
Victor Zeng <zacker150@hotmail.com>
2022-08-24 02:57:05 -0700

    Lowercase

--- Nightbot/so.js
+++ Nightbot/so.js
@@ -1,13 +1,13 @@
 const shoutouts = {
-    "afetalpig": "Check out Caboose, I mean Liv's stream at https://www.twitch.tv/afetalpig	"
+    "afetalpig": "Check out Caboose, I mean Liv's stream at https://www.twitch.tv/afetalpig	",
     "bmmochi": "Check out this just chatting streamer pretending to be an overwatch streamer at twitch.tv/bmmochi.",
     "fitti": "One day, Fitti will stream again. We graciously await that day at twitch.tv/fitti.",
     "lindseybear_": "Check Lindsey out at https://www.twitch.tv/lindseybear_ Just don't get her started on red velvet cake.",
 };
 
 function shoutout(target, displayName, url, game) {
-    if (target in shoutouts) {
-        return shoutouts[target];
+    if (target.toLowerCase() in shoutouts) {
+        return shoutouts[target.toLowerCase()];
     }
     return `Check out ${displayName} at ${url}. They were last playing ${game}`;
 }
