bc7a6c3a0f7bcfe6dd2cd3bfb9547a53a6c07a25
Victor Zeng <zacker150@hotmail.com>
2022-08-24 02:52:43 -0700

    Added displayname

--- Nightbot/so.js
+++ Nightbot/so.js
@@ -1,10 +1,13 @@
 const shoutouts = {
-    "bmmochi": "Check out this just chatting streamer pretending to be an overwatch streamer at twitch.tv/bmmochi."
+    "afetalpig": "Check out Caboose, I mean Liv's stream at https://www.twitch.tv/afetalpig	"
+    "bmmochi": "Check out this just chatting streamer pretending to be an overwatch streamer at twitch.tv/bmmochi.",
+    "fitti": "One day, Fitti will stream again. We graciously await that day at twitch.tv/fitti.",
+    "lindseybear_": "Check Lindsey out at https://www.twitch.tv/lindseybear_ Just don't get her started on red velvet cake.",
 };
 
-function shoutout(target, url, game) {
+function shoutout(target, displayName, url, game) {
     if (target in shoutouts) {
         return shoutouts[target];
     }
-    return `Check out ${target} at ${url}. They were last playing ${game}`;
+    return `Check out ${displayName} at ${url}. They were last playing ${game}`;
 }
