dd7b8d7eafb01fd8542836344b11ec647edc4799
Victor Zeng <zacker150@hotmail.com>
2022-08-24 02:18:15 -0700

    Shoutout command

---
+++ Nightbot/so.js
@@ -0,0 +1,10 @@
+const shoutouts = {
+    "bmmochi": "Check out this just chatting streamer pretending to be an overwatch streamer at twitch.tv/bmmochi."
+}
+
+function shoutout(target) {
+    if (target in shoutouts) {
+        return shoutouts[target]
+    }
+    return "$(twitch $(touser) \"Check out {{displayName}} at {{url}}. They were last playing {{game}}\")"
+}
