98d3c2aeac7bc05a0c84717482b4c6917b2da8b3
Victor Zeng <zacker150@hotmail.com>
2022-08-24 02:58:53 -0700

    Pig

--- Nightbot/so.js
+++ Nightbot/so.js
@@ -1,5 +1,5 @@
 const shoutouts = {
-    "afetalpig": "Check out Caboose, I mean Liv's stream at https://www.twitch.tv/afetalpig	",
+    "afetalpig": "Check out Caboose, I mean Pig's stream at https://www.twitch.tv/afetalpig	",
     "bmmochi": "Check out this just chatting streamer pretending to be an overwatch streamer at twitch.tv/bmmochi.",
     "fitti": "One day, Fitti will stream again. We graciously await that day at twitch.tv/fitti.",
     "lindseybear_": "Check Lindsey out at https://www.twitch.tv/lindseybear_ Just don't get her started on red velvet cake.",
