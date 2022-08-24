ae76e62ad7ca0e85cbcdab889771de3d62bc2ba5
Victor Zeng <zacker150@hotmail.com>
2022-08-24 03:03:27 -0700

    Added iflynick shoutout

--- Nightbot/so.js
+++ Nightbot/so.js
@@ -2,6 +2,7 @@ const shoutouts = {
     "afetalpig": "Check out Caboose, I mean Pig's stream at https://www.twitch.tv/afetalpig	",
     "bmmochi": "Check out this just chatting streamer pretending to be an overwatch streamer at twitch.tv/bmmochi.",
     "fitti": "One day, Fitti will stream again. We graciously await that day at twitch.tv/fitti.",
+    "iflynick": "One day, he will finish his discord bot. Check out iFlyNick at https://www.twitch.tv/iflynick",
     "lindseybear_": "Check Lindsey out at https://www.twitch.tv/lindseybear_ Just don't get her started on red velvet cake.",
 };
 
