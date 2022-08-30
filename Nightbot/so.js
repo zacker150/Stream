cae38ca69ce0453d91c440f8e5f2d233f1ac7469
Victor Zeng <zacker150@hotmail.com>
2022-08-30 00:13:55 -0700

    Fuzh

--- Nightbot/so.js
+++ Nightbot/so.js
@@ -6,7 +6,8 @@ const shoutouts = {
     "iflynick": "One day, he will finish his discord bot. Check out iFlyNick at twitch.tv/iflynick",
     "lindseybear_": "Check Lindsey out at twitch.tv/lindseybear_ Just don't get her started on red velvet cake.",
     "moontsukitsuki": "Grab MoonTsukiTsuki a Kombucha at twitch.tv/moontsukitsuki.",
-    "kiapiaa": "Kia needs attention. twitch.tv/kiapiaa"
+    "kiapiaa": "Kia needs attention. twitch.tv/kiapiaa",
+    "fuzhpuzy": "I hear my twin in Fuzh's channel really likes puns. twitch.tv/fuzhpuzy"
 };
 
 function shoutout(target, displayName, url, game) {
